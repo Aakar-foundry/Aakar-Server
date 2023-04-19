05f8ae4a6718a7835cffb70940c9c5b907e817b7
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-04-19 15:25:24 +0530

    Feature added
    - GenerateOTP
    - GetJwtToken
    - VerifyToken
    - AuthRoute

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -1,7 +1,68 @@
 exports.login=()=>{
     try {
         
-    } catch (error) {
+    } 
+    catch (error) {
+        
+    }
+    finally{
+        
+    }
+}
+
+exports.register=()=>{
+    try {
+        
+    } 
+    catch (error) {
+        
+    }
+    finally{
+        
+    }
+}
+
+exports.verifyUser=()=>{
+    try {
+        
+    } 
+    catch (error) {
+        
+    }
+    finally{
+        
+    }
+}
+
+exports.sendOTP=()=>{
+    try {
+        
+    }
+    catch (error) {
+        
+    }
+    finally{
+        
+    }
+}
+
+exports.forgotPasswordUserVerify=()=>{
+    try {
+        
+    } 
+    catch (error) {
+        
+    }
+    finally{
+        
+    }
+}
+
+exports.forgotPassword=()=>{
+    try {
+        
+    } 
+    catch (error) {
         
     }
     finally{
