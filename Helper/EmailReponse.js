60a182e56911f3c4d9ccbaca761510f3e01e6bc0
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-04-19 17:22:02 +0530

    created new email response templete

--- Helper/EmailReponse.js
+++ Helper/EmailReponse.js
@@ -1,3 +1,6 @@
 exports.NewEmployeeRegisterOTPEmailTemplete = (otp) => {
     return "Here is your OTP to verify your account: " + otp;
-  };
+};
+exports.EmployeeForgotPasswordEmailTemplete = (otp) => {
+    return "Here is your OTP to Change password: " + otp;
+}
