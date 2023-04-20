99b317fb332c3ddc972344925c612cae033e2d10
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-04-20 12:31:36 +0530

    production build fix

--- Config/DB_Config.js
+++ Config/DB_Config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 
-const mongoURL = process.env.MONGODB_URI;
+const mongoURL = process.env.MONGODB_URL;
 const connectionParams={
     useNewUrlParser: true,
     useUnifiedTopology: true 
